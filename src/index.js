8443a6b5db5a065efb08b71eca75262c37ccced0
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2025-08-07 15:59:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 function generatePoem(event){
 event.preventDefault();
 
-let poemElement = document.querySelector("#poem");
-poemElement.innerHTML = "ACAB";
-
 new Typewriter("poem",{
     strings: "ACAB",
     autostart: true,
     cursor: "",
-}
-
-)
+    delay: 10,
+})
 
 }
 
