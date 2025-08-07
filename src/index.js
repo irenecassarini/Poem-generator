77bf8754372100f548254614aa1a4f2ec4f95932
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2025-08-07 16:30:55 +0200

    cha

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: "ACAB",
+function displayPoem(response){
+new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+    let apiKey = "334ff5a8425o317bf171aa4a1a6b3b0t";
+    let context = "Please create a short poem with an anticapitalist perspective";
+    let prompt = "Create a poem about this topic";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
+
+    axios(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
