0aa04f877da43d145e07cdb43bdcd839a9cb7182
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2025-08-07 15:56:55 +0200

    ch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+function generatePoem(event){
+event.preventDefault();
+
+let poemElement = document.querySelector("#poem");
+poemElement.innerHTML = "ACAB";
+alert("generating Poem");
+}
+
 let poemFormElement = document.querySelector("poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
