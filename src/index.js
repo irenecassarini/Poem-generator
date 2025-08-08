2a9bcab606d9bd8e479d5189f66cc38c845719d2
irenecassarini <47857296+irenecassarini@users.noreply.github.com>
2025-08-08 17:03:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,11 +17,10 @@ function generatePoem(event) {
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
  
     let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
+   poemElement.classList.remove("hidden");
+   poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
 
-  axios.get(apiURL).then(displayPoem);
-    axios(apiUrl).then(displayPoem);
+   axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
